6988cddb97f265d18aa397d96da7202b2ed63652
ttruong <truongtuanvu.it@gmail.com>
2016-10-24 11:05:10 +0300

    test

---
+++ src/test/java/tdscProject/features/DefinitionTestSuite.java
@@ -0,0 +1,9 @@
+package tdscProject.features;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/consult_dictionary/Header.feature")
+public class DefinitionTestSuite {}
