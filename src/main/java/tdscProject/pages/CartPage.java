a4881dc932b45b086084a86fed101dd48d1271e0
ttruong <truongtuanvu.it@gmail.com>
2016-11-17 14:16:42 +0200

    123

--- src/main/java/tdscProject/pages/CartPage.java
+++ src/main/java/tdscProject/pages/CartPage.java
@@ -3,11 +3,12 @@ package tdscProject.pages;
 import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.pages.PageObject;
 
-@DefaultUrl("https://tdsc-staging.zaelab.com/tdscstorefront/?site=powertools")
-public class CartPage extends PageObject {
 
+@DefaultUrl("https://zaelab:baleaz@tdsc-staging.zaelab.com/tdscstorefront/?site=powertools")
+public class CartPage extends PageObject {
 
     public void goToWebsite() {
         open();
+
     }
 }
